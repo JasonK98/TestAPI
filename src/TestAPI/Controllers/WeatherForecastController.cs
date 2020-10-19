46f908252f583f2608b1cc4d3cdf256274f47ed9
Jason <JasonK4151@gmail.com>
2020-10-19 11:58:23 +0100

    Changing weather values

--- src/TestAPI/Controllers/WeatherForecastController.cs
+++ src/TestAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TestAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "FreezingCowdenbeath", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
